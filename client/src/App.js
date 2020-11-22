2fd1b0c80afd6a13a7b5ba93228468a12dd80225
YakoobHammouri <yakoob.hammouri@hotmail.com>
2020-11-22 17:39:32 +0200

    get state from store and use App
    Signed-off-by: YakoobHammouri <yakoob.hammouri@hotmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text} from 'react-native';
-
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchDemo} from './actions/demoAction';
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDemo());
+  }, []);
+
+  const users = useSelector((state) => state.demo.users);
   return (
     <>
       <Text>Private Teacher</Text>
+      {users?.map((item, index) => {
+        return <Text key={index}>{item}</Text>;
+      })}
     </>
   );
 };

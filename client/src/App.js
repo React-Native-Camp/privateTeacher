80a5b2e770bdb3b06a37f52fed0d1f490276cb64
YakoobHammouri <yakoob.hammouri@hotmail.com>
2020-11-24 12:09:50 +0200

    using login  screen #23
    Signed-off-by: YakoobHammouri <yakoob.hammouri@hotmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+import LoginScreen from './components/screens/MainScreen/LoginScreen';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+});
 
 const App = () => {
   return (
-    <>
-      <Text>Private Teacher</Text>
-    </>
+    <View style={styles.container}>
+      <LoginScreen />
+    </View>
   );
 };
 

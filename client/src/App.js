66b8b08fb601c02bafe6474a106f1c832963d9b2
nadeen123 <nadeenshaheen.shahin@gmail.com>
2020-11-24 16:57:05 +0200

    create choose type and add it to app page and add logo pic from assetst

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import ChooseType from './components/ChooseType';
 
 const App = () => {
   return (
-    <>
-      <Text>Private Teacher</Text>
-    </>
+    <View style={styles.container}>
+      <ChooseType />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
 
 export default App;
---
+++ client/src/components/ChooseType.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {StyleSheet, View, Image, Button, ToastAndroid} from 'react-native';
+
+import RadioForm from 'react-native-simple-radio-button';
+
+var Types = [
+  {label: 'continue as a sttudent', value: 0},
+  {label: 'continue as a teacher', value: 1},
+];
+
+// function ChooseType({navigation}) {
+class ChooseType extends React.Component {
+  static navigationOptions = {
+    title: 'Search',
+  };
+
+  render() {
+    // const {navigate} = this.props.navigation;
+    return (
+      <View style={styles.main}>
+        <View>
+          <Image style={styles.image} source={require('../assest/logoo.png')} />
+        </View>
+        <View style={styles.container}>
+          <RadioForm
+            radio_props={Types}
+            initial={-1}
+            onPress={value => {
+              ToastAndroid.show(value.toString(), ToastAndroid.SHORT);
+            }}
+            buttonSize={30}
+            buttonOuterSize={40}
+            selectedButtonColor={'#F0AB2A'}
+            selectedLabelColor={'#F0AB2A'}
+            labelStyle={{fontSize: 22}}
+            buttonColor={'#28AE81'}
+          />
+        </View>
+
+        <View style={styles.Button}>
+          <Button
+            title="Confirm"
+            color="#28AE81"
+            // onPress={() => navigate('Search')}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  container: {
+    backgroundColor: '#fff',
+    padding: 10,
+    marginTop: 40,
+    marginLeft: 20,
+  },
+  image: {
+    width: 300,
+    height: 350,
+    alignItems: 'center',
+    marginLeft: 50,
+    marginTop: 20,
+  },
+
+  Button: {
+    padding: 45,
+    marginLeft: 80,
+    width: 250,
+    height: 200,
+    margin: 50,
+  },
+});
+
+export default ChooseType;

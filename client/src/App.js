2f6b148ce402d3e44690fc709dbb918ba3999e11
YakoobHammouri <yakoob.hammouri@hotmail.com>
2020-11-25 22:55:15 +0200

    using home
    Signed-off-by: YakoobHammouri <yakoob.hammouri@hotmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, {useEffect} from 'react';
-import Home from './components/Home';
-import TeacherCourses from './components/screens/AuthScreen/TeacherCourses'
 import {Text, View, StyleSheet} from 'react-native';
 import LoginScreen from './components/screens/MainScreen/LoginScreen';
 import ChooseType from './components/screens/MainScreen/ChooseType';
+import TeacherCourses from './components/screens/AuthScreen/TeacherCourses';
+import HomeScreen from './components/screens/MainScreen/HomeScreen';
 import {useDispatch, useSelector} from 'react-redux';
 import {fetchDemo} from './actions/demoAction';
 
@@ -23,10 +23,11 @@ const App = () => {
   const users = useSelector((state) => state.demo.users);
   return (
     <View style={styles.container}>
-      <TeacherCourses />
+      {/* <TeacherCourses /> */}
       {/* <PersonCard /> */}
       {/* <ChooseType />
       <LoginScreen /> */}
+      <HomeScreen />
       {/* <Text>Private Teacher</Text>
       {users?.map((item, index) => {
         return <Text key={index}>{item}</Text>;

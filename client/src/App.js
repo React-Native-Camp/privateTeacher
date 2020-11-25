7feafa13d3edc41aaca5b9bd444d736d283465cb
YakoobHammouri <yakoob.hammouri@hotmail.com>
2020-11-25 11:43:53 +0200

    confirm screen #29
    Signed-off-by: YakoobHammouri <yakoob.hammouri@hotmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Text} from 'react-native';
+import ConfirmScrren from '../src/components/confirmScreen';
 
 const App = () => {
   return (
     <>
-      <Text>Private Teacher</Text>
+      <ConfirmScrren />
     </>
   );
 };

32873e9919d5333b3a2fbd58d04789271c532ae9
YakoobHammouri <yakoob.hammouri@hotmail.com>
2020-11-29 18:57:40 +0200

    student profile #22
    Signed-off-by: YakoobHammouri <yakoob.hammouri@hotmail.com>

--- client/src/components/screens/AuthScreen/StudentProfile.js
+++ client/src/components/screens/AuthScreen/StudentProfile.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
 import {Input} from 'react-native-elements';
 import PersonCard from '../../PersonCard';
 
 export default function TeacherCourses() {
+  const [isEdit, setIsEdit] = useState(false);
+  const [disabledInput, setDisabledInput] = useState(true);
+  const enableEdit = () => {
+    setIsEdit(!isEdit);
+    setDisabledInput(!disabledInput);
+  };
+
+  const editHandle = () => {
+    setIsEdit(!isEdit);
+    setDisabledInput(!disabledInput);
+    alert('editHandle');
+  };
+
   return (
     <>
-      <PersonCard displayEdit={true} name={'Ahmed Mostafa'} />
+      <PersonCard
+        displayEdit={!isEdit}
+        displaySave={isEdit}
+        enableEdit={enableEdit}
+        name={'Ahmed Mostafa'}
+        editHandle={editHandle}
+      />
       <ScrollView style={styles.Inputs}>
         <Input
-          placeholder="INPUT WITH ICON"
+          placeholder="Your Phone"
           leftIcon={{type: 'MaterialCommunityIcons', name: 'phone'}}
           label="Phone"
-          rightIconContainerStyle={{
-            color: '#28AE81',
-            backgroundColor: '#28AE81',
-            width: 50,
-          }}
+          disabled={disabledInput}
+        />
+
+        <Input
+          placeholder="Your Address"
+          leftIcon={{type: 'FontAwesome5', name: 'home'}}
+          label="Address"
+          disabled={disabledInput}
+        />
+
+        <Input
+          placeholder="Your Age"
+          leftIcon={{name: 'date-range'}}
+          label="Age"
+          disabled={disabledInput}
         />
       </ScrollView>
     </>
